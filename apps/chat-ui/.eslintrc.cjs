c13279164201667a028c32e9e6d4efc0e44a9bb0
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2024-06-03 15:44:47 +0100

    feat: chat-ui

---
+++ apps/chat-ui/.eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"prettier",
+	],
+	plugins: ["@typescript-eslint"],
+	ignorePatterns: ["*.cjs"],
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		},
+	],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2020,
+		extraFileExtensions: [".svelte"],
+	},
+	rules: {
+		"require-yield": "off",
+		"@typescript-eslint/no-explicit-any": "error",
+		"@typescript-eslint/no-non-null-assertion": "error",
+		"@typescript-eslint/no-unused-vars": [
+			// prevent variables with a _ prefix from being marked as unused
+			"error",
+			{
+				argsIgnorePattern: "^_",
+			},
+		],
+		"object-shorthand": ["error", "always"],
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+};
