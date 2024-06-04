40c31345ca1653d6232cbde2ba63c89612691e1a
Ahmed Samady <samadycrm@gmail.com>
2024-06-04 20:23:44 +0100

    fix: container URL

--- apps/api/app/client.py
+++ apps/api/app/client.py
@@ -1,13 +1,13 @@
 from langserve import RemoteRunnable
 import requests
 
-""" model = RemoteRunnable("http://localhost:8000/rag")
+""" model = RemoteRunnable("http://API:8000/rag")
 
 response = model.invoke("Qui est le doyen de la fst tanger?")
 
 print(response) """
 
-url = "http://localhost:8000/rag/stream"
+url = "http://API:8000/rag/stream"
 
 payload = { "input": { "text": "Qui est le doyen de la fst tanger?" } }
 
