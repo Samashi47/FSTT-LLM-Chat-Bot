f5f7eee4c0b75cffc098ee8edfd6ae5ce6ec2b7d
Ahmed Samady <samadycrm@gmail.com>
2024-06-04 20:24:37 +0100

    bug: retrieve context from db

--- apps/api/app/rag_chroma/chain.py
+++ apps/api/app/rag_chroma/chain.py
@@ -4,12 +4,11 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from app.rag_chroma.schema import Question
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-import argparse
 import warnings
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
+CHROMA_PATH = "app/rag_chroma/chroma-fstt"
 PROMPT_TEMPLATE = """
 Répondre seulement en français, si le contexte ne répond pas à la question vouz pouvez spéculer, et mentionner que vous n'êtes pas sûr et demander plus de détails ou d'être plus précis.:
 
@@ -20,16 +19,15 @@ Répondre seulement en français, si le contexte ne répond pas à la question v
 Répondre au question suivant avec le context ci-dessus: {question}
 """
 
-vectorstore = Chroma.from_texts(
-    [""],
-    collection_name="chroma-fstt",
-    embedding=HuggingFaceEmbeddings(model_name="OrdalieTech/Solon-embeddings-large-0.1", model_kwargs = {'device': 'cpu'}),
+vectorstore = Chroma(
+    persist_directory=CHROMA_PATH,
+    embedding_function=HuggingFaceEmbeddings(model_name="OrdalieTech/Solon-embeddings-large-0.1", model_kwargs = {'device': 'cpu'}),
 )
-retriever = vectorstore.as_retriever(search_kwargs={'k': 5})
+retriever = vectorstore.as_retriever(search_kwargs={'k': 7})
 prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
 
 # LLM
-model = Ollama(model="fsttchatbot:latest")
+model = Ollama(model="llama3",base_url= "http://ModelsHandler:11434")
 
 # RAG chain
 chain = (
@@ -37,7 +35,4 @@ chain = (
     | prompt
     | model
     | StrOutputParser()
-)
-
-
-chain = chain.with_types(input_type=Question)
+).with_types(input_type=Question)
